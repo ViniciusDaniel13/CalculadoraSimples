9f4ec20711cc7497c14c7674f5f12dd3e0f6b570
Vinicius <vinicius-daniel90@hotmail.com>
2022-08-05 00:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraSimples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int resultado;
+            char sinal;
+
+            Console.WriteLine("Insira o primeiro número:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira um sinal(+,-,/,*)");
+            sinal = char.Parse(Console.ReadLine());
+
+            switch (sinal)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    Console.WriteLine(resultado);
+                    break;
+
+                case '-':
+                    resultado = num1 - num2;
+                    Console.WriteLine(resultado);
+                    break;
+
+                case '/':
+                    resultado = num1 / num2;
+                    Console.WriteLine(resultado);
+                    break;
+
+                case '*':
+                    resultado = num1 * num2;
+                    Console.WriteLine(resultado);
+                    break;
+
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
